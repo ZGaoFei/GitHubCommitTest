0ff0248ae674caaffa16620829a1bf7afb8b607e
ZGaoFei <m18317713325@163.com>
2016-07-20 10:42:52 +0800

    First commit

--- app/src/main/java/reviewdemo/hbl/com/githubcommittest/NextClass.java
+++ app/src/main/java/reviewdemo/hbl/com/githubcommittest/NextClass.java
@@ -4,4 +4,31 @@ package reviewdemo.hbl.com.githubcommittest;
  * Created by KevinYoung on 2016/6/20.
  */
 public class NextClass {
+    private String name;
+    private String sex;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
